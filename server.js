330bae0db3687eccca021f583368458e804f009e
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-06-11 19:53:51 +0530

    add defalt route in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import helmet from 'helmet';  
 
 
-import connectDB from './config/db.js';
+import connectDB from './config/mongodb.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 
@@ -20,6 +20,9 @@ connectDB();
 // parse incoming JSON requests
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
