335e93a134f7f61d17ff263f6314a9c3fbfd0b1f
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-06-11 15:10:31 +0530

    change with ES6+ syntax

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,//tells Mongoose to use the new and improved URL parser
-      useUnifiedTopology: true//Enables the new topology engine
-    });
-    console.log(' MongoDB connected');// if connection success
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');// if connection is successful
   } catch (err) {
-    console.error(' MongoDB connection error:', err.message);// if connection fails
-    process.exit(1); // Exit process with failure
+    console.error(' MongoDB connection failed:', err.message); // if connection fails
+    process.exit(1);// Exit process with failure
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
